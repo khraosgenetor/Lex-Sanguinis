0cf6f2db56c6aa3d0c774c20ca002e0465a3435f
Khraos Genetor <khraos@khraos.in>
2026-02-16 22:03:09 +0530

    Update LexSanguinis.java

--- src/main/java/in/khraos/lexsanguinis/LexSanguinis.java
+++ src/main/java/in/khraos/lexsanguinis/LexSanguinis.java
@@ -22,6 +22,8 @@ public class LexSanguinis { // Working on lore rn, will update classes (DnD styl
                             // Also, will be adding more items, and maybe some blocks, entities, etc. in the future.
                             // For now, just want to get the basic structure of the mod set up and then will start adding content. (11th Feb n 12th Feb n 15th Feb)
 
+    // I'm going insane
+
     // Mod ID for reference
     public static final String MODID = "lexsanguinis";
 
